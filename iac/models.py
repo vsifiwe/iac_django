a50901d55d74c5d21ad8753bc0f2021e7b3914f0
Manzi <asifiwemanzi@gmail.com>
2023-02-14 17:38:20 +0200

    database schema generation

--- iac/models.py
+++ iac/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    district = models.CharField(max_length=50)
+    sector = models.CharField(max_length=50)
+    road_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+class Device(models.Model):
+    device_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+class Appointments(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    def __str__(self) -> str:
+        return super().__str__()
